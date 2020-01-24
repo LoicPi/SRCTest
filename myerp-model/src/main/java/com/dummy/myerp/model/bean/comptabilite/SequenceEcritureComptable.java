9206fcad1236a25b67d7c8a274fbfb9ca17900ca
Loic <lcpiriou@gmail.com>
2020-01-24 11:19:19 +0100

    Ajout du paramètre codeJournal et des méthodes hashcode et equals

--- myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/SequenceEcritureComptable.java
+++ myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/SequenceEcritureComptable.java
@@ -7,6 +7,8 @@ package com.dummy.myerp.model.bean.comptabilite;
 public class SequenceEcritureComptable {
 
     // ==================== Attributs ====================
+	/** Le code du journal **/
+	private String codeJournal;
     /** L'année */
     private Integer annee;
     /** La dernière valeur utilisée */
@@ -25,34 +27,68 @@ public class SequenceEcritureComptable {
      * @param pAnnee -
      * @param pDerniereValeur -
      */
-    public SequenceEcritureComptable(Integer pAnnee, Integer pDerniereValeur) {
-        annee = pAnnee;
+    public SequenceEcritureComptable(String pCodeJournal, Integer pAnnee, Integer pDerniereValeur) {
+        codeJournal = pCodeJournal;
+    	annee = pAnnee;
         derniereValeur = pDerniereValeur;
     }
 
 
     // ==================== Getters/Setters ====================
+    
+    public String getCodeJournal() {
+		return codeJournal;
+	}
     public Integer getAnnee() {
         return annee;
-    }
-    public void setAnnee(Integer pAnnee) {
-        annee = pAnnee;
     }
     public Integer getDerniereValeur() {
         return derniereValeur;
     }
-    public void setDerniereValeur(Integer pDerniereValeur) {
-        derniereValeur = pDerniereValeur;
+	public void setCodeJournal(String codeJournal) {
+		this.codeJournal = codeJournal;
+	}
+	public void setAnnee(Integer pAnnee) {
+        annee = pAnnee;
     }
+	public void setDerniereValeur(Integer pDerniereValeur) {
+        derniereValeur = pDerniereValeur;
+    } 
 
+	// ==================== HashCode/Equals ====================
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codeJournal == null) ? 0 : codeJournal.hashCode());
+		return result;
+	}
 
-    // ==================== Méthodes ====================
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SequenceEcritureComptable other = (SequenceEcritureComptable) obj;
+		if (codeJournal == null) {
+			if (other.codeJournal != null)
+				return false;
+		} else if (!codeJournal.equals(other.codeJournal))
+			return false;
+		return true;
+	}
+
+	// ==================== Méthodes ====================
     @Override
     public String toString() {
         final StringBuilder vStB = new StringBuilder(this.getClass().getSimpleName());
         final String vSEP = ", ";
         vStB.append("{")
-            .append("annee=").append(annee)
+        	.append("codeJournal=").append(codeJournal)
+            .append(vSEP).append("annee=").append(annee)
             .append(vSEP).append("derniereValeur=").append(derniereValeur)
             .append("}");
         return vStB.toString();
